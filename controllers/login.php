8d13a71ba63af4205e1da82d0fce756769303521
unknown <merleveurink@hotmail.com>
2018-12-21 14:53:58 +0100

    progress login

---
+++ controllers/login.php
@@ -0,0 +1,16 @@
+<?php
+
+use core\database;
+
+
+class login
+{
+    public function Login()
+    {
+        $content = new \models\login();
+        require_once('views/login.phtml');       
+    }
+    
+}
+
+?>

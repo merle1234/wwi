ca814b8c211820801c8affe2e825c2452d38897a
VosLeon <leonvos@windesheim.nl>
2018-12-24 00:27:47 +0100

    product page

--- models/Products.php
+++ models/Products.php
@@ -7,7 +7,7 @@ class Products
         $conn=new  database();
         
        
-        $sql = "SELECT stockitemname FROM stockitems";
+        $sql = "SELECT stockitemname,unitprice FROM stockitems where IsChillerStock = 1 ORDER BY stockitemname ASC";
         $result = $conn->query($sql);
        
         return $result;

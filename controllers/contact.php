81cb704163d8992ea34a396fec2d5330282dc5ff
unknown <woutervdriel@live.nl>
2019-01-04 17:03:17 +0100

    Contactformulier + einde

--- controllers/contact.php
+++ controllers/contact.php
@@ -1,10 +1,18 @@
 <?php
   class contact
   {
-      public function contactformulier();
+      public function contactformulier()
   {
     
     require_once('views/contact.phtml'); 
+  }
+  }
+  class contactform
+  {
+      public function contactform()
+  {
+    
+    require_once('views/contactform.phtml'); 
   }
 }
     ?>

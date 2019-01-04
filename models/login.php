465a860dbd73cb21a26a92a759b505861e5ef252
unknown <merleveurink@hotmail.com>
2019-01-04 11:23:14 +0100

    progress login

--- models/login.php
+++ models/login.php
@@ -2,33 +2,25 @@
 namespace models;
 use core\database;
 
-
-class login {
-
-    private $data;
-
-    function __constructor() {}
-    /*functions */
-    
-    function load($url){
-        include($url);
+class Login_model{
+    function __construct($conn){
+        $this->conn = $conn;
     }
+    function check_user_and_pass(){
+        $select_user_pass_query = $this->conn->prepare("SELECT username, password FROM users WHERE username = :username, password = :password");
 
-    /*Get / Set DATA  */
-
-    function setData($name, $value){
-        $this->data[$name] = htmlentities($value, ENT_QUOTES);
-    }
-
-    function getData($name) {
-        if (isset($this->data[$name])) {
-            return $this->data[$name];
-            else 
-            {
-                return '';
+        if($select_user_pass_query->execute(array(':username'=>$_GET["username"], ':password'=>$_GET["password"]))){
+            $details = $select_user_pass_query->fetch(PDO::FETCH_NUM);
+            if(!empty($details)){
+                return true;
+            }else{
+                Login_view::show_message("Wrong user or password");
             }
+        }else{
+            Login_view::show_message("Error occured");
         }
     }
+}
 
-} 
+
 ?>

ac0bda44e81cb8b8d4cb3fe282a82d0597fbe99e
unknown <stefan.dedden@live.nl>
2019-01-08 12:40:15 +0100

    sad

--- models/registratie.php
+++ models/registratie.php
@@ -5,8 +5,7 @@ use core\database;
 class Registratie {
 
 public function StoreData($email,$password)
-    {
-        
+    {       
         $connection = new database();       
         $query = $connection->insert("INSERT INTO account(Password, EmailAddress)
         VALUES ('$password' , ' $email' )");

4609c38f6777ddbfc25832c7c1ded7e25b511dcb
Rutenbarg <s.ruitenberg@live.nl>
2019-01-08 13:21:36 +0100

    Aanpassing home

--- models/content.php
+++ models/content.php
@@ -9,16 +9,11 @@ class content extends products
 
         $conn = new database();
 
-<<<<<<< HEAD
-        $query = $connection->query("SELECT * FROM artikelen");
-        
-        return $query;
-=======
+
         $query = "SELECT stockitemID, stockitemname, Photo FROM stockitems ORDER BY stockitemname ASC";
         $queryresult = $conn->query($query);
 
         return $queryresult;
->>>>>>> 46ebc141adeac2f9664558a08993ae85d85ca18f
     }
 
 }

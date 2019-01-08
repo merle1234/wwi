e7bc2ea6eacfa6b05e2051321f457c934137285c
unknown <merleveurink@hotmail.com>
2019-01-08 19:47:56 +0100

    progress

--- models/login.php
+++ models/login.php
@@ -8,6 +8,7 @@ public function getlogin($mail, $password)
 {
 // here goes some hardcoded values to simulate the database
 if(isset($mail) && isset($password)){
+    $query = "SELECT $username, $password FROM users"
 if($mail=='admin' && $password=='admin'){
 return 'login';
 }

5f37fc474d5f881fefb3bfc0007c9a83991eec31
unknown <stefan.dedden@live.nl>
2019-01-10 18:38:04 +0100

    s

--- models/registratie.php
+++ models/registratie.php
@@ -10,7 +10,6 @@ public function StoreData($email,$password,$naam,$adres,$postcode,$stad,$tel)
         $connection = new database();
         $nawquery = $connection->insert("INSERT INTO users(FullName, AdressLine, PostalCode, PhoneNumber, City)
         VALUES ('$naam' , '$adres' , '$postcode' , '$tel' , '$stad' )");
-        var_dump($nawquery);
         $accountquery = $connection->insert("INSERT INTO account(UserId, Password, EmailAddress)
         VALUES ('$nawquery' , '$password' , '$email' )");
        
